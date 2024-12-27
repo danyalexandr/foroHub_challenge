41a4ca959e973a5c94ca5de3ce82ebca3e8b64c5
danyalexandr <danyalexandr@gmail.com>
2024-12-27 00:02:01 -0300

    primer commit

---
+++ src/main/java/com/aluracursos/challenge/forohub_challenge/ForoHub_challenge.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.aluracursos.challenge.forohub_challenge;
+
+/**
+ *
+ * @author lowprioritygarbage
+ */
+public class ForoHub_challenge {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
